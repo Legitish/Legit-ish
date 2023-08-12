31c114eac8fae50296118c2c6a4987bb7ae80f7c
jamesrahhh <xxadmiralxxmlg@gmail.com>
2023-08-11 22:30:57 -0500

    Bedwars alerts working now

--- src/main/java/legitish/module/modules/minigames/BedwarsAlerts.java
+++ src/main/java/legitish/module/modules/minigames/BedwarsAlerts.java
@@ -9,9 +9,10 @@ import legitish.module.modulesettings.impl.ModuleDesc;
 import legitish.module.modulesettings.impl.ModuleTickSetting;
 import legitish.utils.GameUtils;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemFireball;
-import net.minecraft.item.ItemSword;
+import net.minecraft.item.*;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionHelper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,7 +20,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class BedwarsAlerts extends Module {
-    public static ModuleTickSetting sound, fireball, diaSword;
+    public static ModuleTickSetting sound, fireball, diaSword, invis;
     public final HashMap<EntityPlayer, Item> players = new HashMap<>();
 
     public BedwarsAlerts() {
@@ -28,6 +29,7 @@ public class BedwarsAlerts extends Module {
         this.registerSetting(sound = new ModuleTickSetting("Play sound", true));
         this.registerSetting(new ModuleDesc("Alert when player holding"));
         this.registerSetting(fireball = new ModuleTickSetting("Fireball", true));
+        this.registerSetting(diaSword = new ModuleTickSetting("Invis", true));
         this.registerSetting(diaSword = new ModuleTickSetting("Diamond Sword", false));
     }
 
@@ -41,12 +43,17 @@ public class BedwarsAlerts extends Module {
                 }
                 Item item = en.getHeldItem().getItem();
                 if (players.get(en) != item) {
-                    if (item instanceof ItemFireball) {
+                    if (Item.getIdFromItem(item) == 385 && fireball.isToggled()) {
                         Notifications.sendNotification(Notifications.NotificationTypes.WARN, en.getName() + " is holding a Fireball! [" + Math.round(en.getDistanceToEntity(mc.thePlayer)) + "m]", 5000);
                         mc.thePlayer.playSound("note.pling", 1.0F, 1.0F);
-                    } else if (item instanceof ItemSword) {
-                        Notifications.sendNotification(Notifications.NotificationTypes.WARN, en.getName() + " is holding a Diamond Sword! [" + Math.round(en.getDistanceToEntity(mc.thePlayer)) + "m]", 5000);
+                    } else if (Item.getIdFromItem(item) == 276 && diaSword.isToggled()) {
+                        Notifications.sendNotification(Notifications.NotificationTypes.WARN, en.getName() + " is holding a Diamond Sword! [" + Math.round(en.getDistanceToEntity(mc.thePlayer)) + "m]" + item.getUnlocalizedName(), 5000);
                         mc.thePlayer.playSound("note.pling", 1.0F, 1.0F);
+                    } else if (item instanceof ItemPotion) {
+                        if (mc.thePlayer.getHeldItem().getItemDamage() == 8206 && invis.isToggled()) {
+                            Notifications.sendNotification(Notifications.NotificationTypes.WARN, en.getName() + " is holding a Diamond Sword! [" + Math.round(en.getDistanceToEntity(mc.thePlayer)) + "m]" + item.getUnlocalizedName(), 5000);
+                            mc.thePlayer.playSound("note.pling", 1.0F, 1.0F);
+                        }
                     }
                     players.remove(en);
                 }
