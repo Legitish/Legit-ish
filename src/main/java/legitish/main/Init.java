75d5e93f1b2b9f62e9a9d3ce834598718f70ae1b
Zircta <hyeoski+zircta@gmail.com>
2023-08-28 16:54:03 -0400

    Readability

--- src/main/java/legitish/main/Init.java
+++ src/main/java/legitish/main/Init.java
@@ -3,11 +3,17 @@ package legitish.main;
 import net.weavemc.loader.api.ModInitializer;
 import net.weavemc.loader.api.event.EventBus;
 import net.weavemc.loader.api.event.StartGameEvent;
+import net.weavemc.loader.api.event.SubscribeEvent;
 
 public class Init implements ModInitializer {
     // If I put ANYTHING else in the preInit() function the mod doesnt initialize for some reason ??????
     @Override
     public void preInit() {
-        EventBus.subscribe(StartGameEvent.Post.class, startGameEvent -> Legitish.init());
+        EventBus.subscribe(this);
+    }
+
+    @SubscribeEvent
+    public void onGameStart(StartGameEvent.Post e) {
+        Legitish.init();
     }
 }
