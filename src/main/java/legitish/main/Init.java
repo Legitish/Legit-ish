aa4166dd7d8dd5b2e96b66136226129f5979b585
Zircta <hyeoski+zircta@gmail.com>
2023-09-03 11:40:45 -0400

    Stop blink sending like 10 notifications

--- src/main/java/legitish/main/Init.java
+++ src/main/java/legitish/main/Init.java
@@ -14,17 +14,23 @@ public class Init implements ModInitializer {
     @SubscribeEvent
     public void onGameStart(StartGameEvent.Post e) {
         Legitish.init();
-        ModuleManager.blink.disable();
+        if(ModuleManager.blink.isEnabled()) {
+            ModuleManager.blink.disable();
+        }
     }
 
     @SubscribeEvent
     public void onShutdown(ShutdownEvent e) {
-        ModuleManager.blink.disable();
+        if(ModuleManager.blink.isEnabled()) {
+            ModuleManager.blink.disable();
+        }
     }
 
     @SubscribeEvent
     public void onWorldLoad(WorldEvent e) {
-        ModuleManager.blink.disable();
+        if(ModuleManager.blink.isEnabled()) {
+            ModuleManager.blink.disable();
+        }
     }
 
     // Note: blink is disabled on game start, shutdown, and world load to avoid it bugging out, please do not remove unless you plan to use a better implementation.
